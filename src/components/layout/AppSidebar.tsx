61010dd4d555a50afc2f9429596dda86b5ab44cf
MaxPrime7097 <156826695+MaxPrime7097@users.noreply.github.com>
2025-10-01 21:30:22 +0100

    Update

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -49,8 +49,6 @@ const quickActions = [
 
 const utils = [
   {title: "À propos", url: "/cs-inc/about", icon: Info },
-  {title: "Politique de confidentialité", url: "/cs-inc/policies/privacy", icon: BookLock },
-  {title: "Conditions d'utilisation", url: "/cs-inc/policies/conditions", icon: BookUser },
   {title: "Aide", url: "/cs-inc/contact", icon: CircleQuestionMark },
 ];
 
