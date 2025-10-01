ea66da95d5f45fafdfcef12781886bdda2a2c013
MaxPrime7097 <156826695+MaxPrime7097@users.noreply.github.com>
2025-10-01 21:27:27 +0100

    Update

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -48,12 +48,11 @@ const quickActions = [
 ];
 
 const utils = [
-  {title: "À propos", url: "/cs-inc/about", icon: Info }
-  {title: "Politique de confidentialité", url: "/cs-inc/policies/privacy", icon: BookLock }
-  {title: "Conditions d'utilisation", url: "/cs-inc/policies/conditions", icon: BookUser }
-  {title: "Aide", url: "/cs-inc/contact", icon: CircleQuestionMark }
-
-]
+  {title: "À propos", url: "/cs-inc/about", icon: Info },
+  {title: "Politique de confidentialité", url: "/cs-inc/policies/privacy", icon: BookLock },
+  {title: "Conditions d'utilisation", url: "/cs-inc/policies/conditions", icon: BookUser },
+  {title: "Aide", url: "/cs-inc/contact", icon: CircleQuestionMark },
+];
 
 export function AppSidebar() {
   const { state } = useSidebar();
